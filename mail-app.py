b0b176fc423cfd262d3824dc4a534fc2c90f8678
Gary Sparks <gsparks@sitespect.com>
2023-02-20 12:58:53 -0500

    feat: Made pages resizable

--- mail-app.py
+++ mail-app.py
@@ -5,7 +5,7 @@ import keyring
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5 import QtWebEngineWidgets
-from PyQt5.QtWidgets import QAction, QMenu, QMainWindow, QWidget, QApplication, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QAction, QMenu, QMainWindow, QWidget, QApplication, QVBoxLayout, QMessageBox, QSplitter
 from PyQt5.QtGui import QIcon, QDesktopServices
 from PyQt5.QtCore import QUrl
 
@@ -58,15 +58,23 @@ class Browser(QMainWindow):
         self.gmail.load(QUrl("https://mail.google.com/mail/u/0/#inbox"))
         self.calendar.load(QUrl("https://calendar.google.com/calendar/u/0/r?pli=1"))
 
+        # Create a QSplitter and add the two QWebEngineViews to it
+        splitter = QSplitter(Qt.Vertical)
+        splitter.addWidget(self.gmail)
+        splitter.addWidget(self.calendar)
+
+        # Set the QSplitter as the central widget of the main window
+        self.setCentralWidget(splitter)
+
         # Create a vertical layout and add the two QWebEngineViews to it
-        layout = QVBoxLayout()
-        layout.addWidget(self.gmail)
-        layout.addWidget(self.calendar)
+        #layout = QVBoxLayout()
+        #layout.addWidget(self.gmail)
+        #layout.addWidget(self.calendar)
 
         # Set the layout as the central widget of the main window
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        #central_widget = QWidget()
+        #central_widget.setLayout(layout)
+        #self.setCentralWidget(central_widget)
 
         # Set the window properties
         self.setWindowTitle("Google Mail and Calendar")
